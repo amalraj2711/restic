96db2219af4aa5ad143fde3c377d00446b367719
amalraj2711 <68366668+amalraj2711@users.noreply.github.com>
2020-07-16 14:33:28 +0530

    Create restic-env.sh

---
+++ restic-env.sh
@@ -0,0 +1,5 @@
+export RESTIC_REPOSITORY="s3:https://s3.amazonaws.com/<bucketname>"
+export AWS_ACCESS_KEY_ID="AKIXXXXXXXXXWD"
+export AWS_SECRET_ACCESS_KEY="a/11Mzj8XXXXXXXXXXXNwzfqvJ"
+export RESTIC_PASSWORD="4lXXXXXXXXm"
+export AWS_DEFAULT_REGION="us-east-1"
